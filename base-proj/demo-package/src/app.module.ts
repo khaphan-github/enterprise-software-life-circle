c123642047b2bba2f17f6dc88d98b09db2cd28cb
khaphan-github <phanhoangkha01@gmail.com>
2025-03-28 15:16:39 +0000

    Feat: demo project

---
+++ base-proj/demo-package/src/app.module.ts
@@ -0,0 +1,41 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CQRSAuthenticationRBAC } from 'cqrs-authentication-rbac';
+
+@Module({
+  imports: [
+    CQRSAuthenticationRBAC.register({
+      dbConf: {
+        host: process.env.PG_MAIN_DB_HOST || 'localhost',
+        port: parseInt(process.env.PG_MAIN_DB_PORT || '5432', 10),
+        user: process.env.PG_MAIN_DB_USER || 'postgres',
+        password: process.env.PG_MAIN_DB_PASSWORD || 'postgres',
+        database: process.env.PG_MAIN_DB_DATABASE || 'postgres',
+      },
+      jwtOptions: {
+        secret: process.env.JWT_SECRET || 'defaultSecret',
+        signOptions: { expiresIn: '60s' },
+      },
+      rbacConf: {
+        authSecretKey: process.env.AUTH_SECRET_KEY || 'defaultAuthSecretKey',
+        authSalt: process.env.AUTH_SALT || 'defaultAuthSalt',
+        authJwtSecret: process.env.AUTH_JWT_SECRET || 'defaultAuthJwtSecret',
+        authAccessTokenSecretKey:
+          process.env.AUTH_ACCESS_TOKEN_SECRET_KEY ||
+          'defaultAccessTokenSecretKey',
+        authRefreshTokenSecretKey:
+          process.env.AUTH_REFRESH_TOKEN_SECRET_KEY ||
+          'defaultRefreshTokenSecretKey',
+        authAccessTokenExpiresIn:
+          process.env.AUTH_ACCESS_TOKEN_EXPIRES_IN || '3600s',
+        authRefreshTokenExpiresIn:
+          process.env.AUTH_REFRESH_TOKEN_EXPIRES_IN || '86400s',
+      },
+      constroller: { enable: true },
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
