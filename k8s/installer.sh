b39da68c9e39aca596c657c3078823152ae273c3
khaphan-github <phanhoangkha01@gmail.com>
2025-03-30 15:33:34 +0000

    Feat: add Kubernetes installation scripts and configuration files; include containerd service definition

--- k8s/installer.sh
+++ k8s/installer.sh
@@ -1,20 +1,11 @@
-# https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/
+#!/bin/bash
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl gpg
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.32/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.32/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
-
 sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
-
 sudo kubeadm init --config kubeadm-config.yaml --v=5
-
 sudo systemctl enable --now kubelet
-
 kubeadm init --config kubeadm-config.yaml
-
-# Commands
-- Reset kubeadm: sudo kubeadm reset & rm -rf ~/.kube
-
